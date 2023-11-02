4b6c22acad0674f0f4bb7315d40a48428f1a797a
somumane <106975847+somumane@users.noreply.github.com>
2023-11-03 01:02:32 +0530

    Url

--- src/pages/api/checkout_sessions/index.js
+++ src/pages/api/checkout_sessions/index.js
@@ -23,8 +23,8 @@ export default async function handler(req, res) {
         mode:"payment",
         payment_method_types:['card'],
         line_items:items,
-        success_url:`${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url:`${req.headers.origin}/cart`       
+        success_url:`https://www.rsb-sweets-qjkg.vercel.app/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url:`https://www.rsb-sweets-qjkg.vercel.app/cart`       
        })
        res.status(200).json(session)
     }
